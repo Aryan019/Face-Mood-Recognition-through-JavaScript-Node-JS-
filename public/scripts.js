91ca010fe2e434c35836e3a1e92209943cce7a1c
Aryan019 <aryan19v@gmail.com>
2024-06-19 10:44:23 +0530

    Adding in the video stream

--- public/scripts.js
+++ public/scripts.js
@@ -4,6 +4,14 @@ const run = async()=>{
     //we need to load our models
     //loading the models is going to use await
 
+    // Getting the web cam and the audio devices ready 
+    const stream = await navigator.mediaDevices.getUserMedia({
+        video:true,
+        audio: false
+    })
+
+    const videoFeedEl = document.getElementById('video-feed')
+    videoFeedEl.srcObject = stream
 }
 
 run()
--- server.js
+++ server.js
@@ -5,3 +5,5 @@ const app = express()
 //serve our static files
 app.use(express.static('public')) 
 app.listen(5000)
+
+console.log("server started at 5000 :")
