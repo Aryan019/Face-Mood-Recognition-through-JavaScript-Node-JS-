4a81dc682c68cc8544c5a10be3c1d2006de76ed5
Aryan019 <aryan19v@gmail.com>
2024-06-19 11:15:21 +0530

    Adding in the Models

--- public/scripts.js
+++ public/scripts.js
@@ -12,6 +12,31 @@ const run = async()=>{
 
     const videoFeedEl = document.getElementById('video-feed')
     videoFeedEl.srcObject = stream
+
+
+    // Fetching in the models
+    await Promise.all([
+        faceapi.nets.ssdMobilenetv1.loadFromUri('./models'),
+        faceapi.nets.faceLandmark68Net.loadFromUri('./models'),
+        faceapi.nets.faceRecognitionNet.loadFromUri('./models'),
+        faceapi.nets.ageGenderNet.loadFromUri('./models'),
+        faceapi.nets.faceExpressionNet.loadFromUri('./models')
+    ])
+
+    // Setting up the canvas
+    const canvas = document.getElementById('canvas')
+    canvas.style.left = videoFeedEl.offsetLeft
+    canvas.style.top = videoFeedEl.offsetTop
+    canvas.style.left = videoFeedEl.offsetLeft
+    canvas.height = videoFeedEl.height
+    canvas.width = videoFeedEl.width
+
+
+    // FacicAL detection with points 
+    setInterval(async()=>{
+        let FaceAIData = await faceapi.detectAllFaces(videoFeedEl).withFaceLandmarks().withFaceDescriptors().withAgeAndGender()
+        console.log(FaceAIData)
+    },200)
 }
 
 run()
